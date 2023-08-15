e33a9eacd8183513c83a4ae98213337665a4b87c
Marcus Horne <marc.horne@amorelie.com>
2023-08-15 16:36:17 +0100

    increase roll bias

--- utilities/index.ts
+++ utilities/index.ts
@@ -18,7 +18,7 @@ export const buildNarratorList = (text: string) => {
 
 export const weightedRandom = () => {
 	let max = 20;
-	let bias = 1.1;
+	let bias = 1.2;
 
 	let randomNumber = Math.floor((1 - Math.pow(Math.random(), bias)) * max) + 1;
 
